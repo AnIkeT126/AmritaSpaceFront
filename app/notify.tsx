d0c53c58b867a091c1126573456bf6a232ae3a82
Aniket Mishra <82095877+AnIkeT126@users.noreply.github.com>
2024-03-18 21:26:11 +0530

    Update notify.tsx

--- app/notify.tsx
+++ app/notify.tsx
@@ -1,18 +1,1 @@
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
-export function notify() {
-    alert('shivemdra')
-    <ToastContainer
-    position="bottom-right"
-    autoClose={5000}
-    hideProgressBar={false}
-    newestOnTop={false}
-    closeOnClick
-    rtl={false}
-    pauseOnFocusLoss
-    draggable
-    pauseOnHover
-    theme="light"
-    />
-}
